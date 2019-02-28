493462cf1cb5a7160939607d33e8f5cca7509e4a
venkat venky <venkyjob452@gmail.com>
2019-02-28 15:08:23 +0530

    structure commit

---
+++ core/database/db.php
@@ -0,0 +1,13 @@
+<?php
+$DSN = "mysql:host=localhost;dbname=car";
+$username = "root";
+$password = "";
+$options = array(PDO::ATTR_PERSISTENT => true);
+try {
+    $pdo = new PDO($DSN, $username, $password, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "connectiond";
+} catch (PDOException $ex) {
+    echo "Database is not connected!.. ".$ex->getMessage();
+}
+?>
